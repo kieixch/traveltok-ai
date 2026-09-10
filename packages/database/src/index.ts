ff2d65218d4a9c174179858a0e8f1498e8ce4e29
kieixch <kieiiixch@gmail.com>
2026-09-11 06:00:18 +0700

    strip NestJS Prisma service from database package

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,5 +1,3 @@
-export { PrismaService } from "./prisma.service";
-export { DatabaseModule } from "./database.module";
 export { PrismaClient } from "./generated/client";
 export * from "./generated/client";
 export * from "./generated/models";
